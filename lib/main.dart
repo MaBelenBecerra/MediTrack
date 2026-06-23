64ac104f53b82c9e5efd2e6ccefca4a3283cd3b5
Maria Belen Becerra <belustianabecerra@gmail.com>
2026-06-22 21:07:35 -0400

    feat: inicialización de Firebase Core conectada al plan Blaze

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'core/utils/notification_helper.dart';
 import 'data/models/medication_model.dart';
@@ -12,6 +13,7 @@ import 'views/auth/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await NotificationHelper.init();
   
   await Hive.initFlutter();
